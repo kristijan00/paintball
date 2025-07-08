594c3470eb7563ecbca141129f1827527a0e22a8
Kristijan Đaković <kristijandjakovic10@gmail.com>
2025-07-08 13:22:54 +0200

    Adjusted the sitemap config and add robots.txt and sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://paintball-seven.vercel.app/',
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 };
